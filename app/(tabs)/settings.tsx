a9e283581f9a97f8110ad0211917015d0c0a1f20
Alema Edrick <alemaedrick85@gmail.com>
2025-07-05 15:00:51 +0300

    chore: update dependencies and add Tailwind CSS configuration
    - Added `nativewind` for Tailwind CSS support.
    - Updated `react-native-safe-area-context` to use caret versioning.
    - Added `tailwindcss` and `prettier-plugin-tailwindcss` to devDependencies.
    - Created a new `tailwind.config.js` file for Tailwind CSS configuration.
    - Updated `tsconfig.json` to include `nativewind-env.d.ts`.

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const HomeTabScreen = () => {
+  return (
+    <View className='flex-1 items-center justify-center bg-white dark:bg-gray-900'>
+      <Text className='text-red-600 text-center'>explore</Text>
+    </View>
+  );
+};
+
+export default HomeTabScreen;
